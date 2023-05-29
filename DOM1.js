07aa734d07376a3ceaef14259f8ce260fd27afcf
lferreram <lferreram@yahoo.com>
2023-05-28 19:28:13 -0600

    first commit

---
+++ DOM1.js
@@ -0,0 +1,5 @@
+const elemento = document.getElementById('Crar un texto por medio de DOM')
+console.log(elemento);
+
+const elemento1 =document.getElementById("Un link a la pagina de google.com")
+console.log(elemento1)
